76e676b2e34387b4ea980e135b91ff0885bd9a5e
totone-uy1990 <ronalggm@outlook.com>
2026-04-01 14:03:09 -0300

    learned path params && query params

--- src/test/java/auth/AuthToken.java
+++ src/test/java/auth/AuthToken.java
@@ -8,83 +8,112 @@ import org.testng.annotations.Test;
 import io.restassured.response.Response;
 
 public class AuthToken {
-
-    @Test
-    public void testObtenerToken() {
-
-        String body = """
-                {
-                 "clientName": "tutonil",
-                 "clientEmail": "Tutoni@gmail.com"
-                }
-                    """;
-
-        given().baseUri("https://simple-books-api.click")
-                .basePath("/api-clients/")
-                .contentType(ContentType.JSON)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(201)
-                .log().all();
-    }
-
-    String token="fbe20bd25813b1028843eb3babaf72a57ca2548c73ebfa55f67c722ecd9e88c3";
-
-
-//submit an order with the token obtained in the previous test
+        // URL REFERENCE:
+        // https://github.com/vdespa/introduction-to-postman-course/blob/main/simple-books-api.md
         @Test
-public void testSubmitOrder() {
-        String body = """
-                {
-                 "bookId": 1,
-                 "customerName": "Tutoni"
-                }
-                    """;
+        public void testGetToken() {
 
-        given().baseUri("https://simple-books-api.click")
-                .basePath("/orders")
-                .contentType(ContentType.JSON)
-                .header("Authorization", "Bearer " + token)
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(201)
-                .log().all();
-    }
+                String body = """
+                                {
+                                 "clientName": "tutonil",
+                                 "clientEmail": "Tutoni@gmail.com"
+                                }
+                                    """;
 
+                given().baseUri("https://simple-books-api.click")
+                                .basePath("/api-clients/")
+                                .contentType(ContentType.JSON)
+                                .body(body)
+                                .when()
+                                .post()
+                                .then()
+                                .statusCode(201)
+                                .log().all();
+        }
 
+        String token = "fbe20bd25813b1028843eb3babaf72a57ca2548c73ebfa55f67c722ecd9e88c3";
 
+        // submit an order with the token obtained in the previous test
+        @Test
+        public void testSubmitOrder() {
+                String body = """
+                                {
+                                 "bookId": 1,
+                                 "customerName": "Tutoni"
+                                }
+                                    """;
 
+                given().baseUri("https://simple-books-api.click")
+                                .basePath("/orders")
+                                .contentType(ContentType.JSON)
+                                .header("Authorization", "Bearer " + token)
+                                .body(body)
+                                .when()
+                                .post()
+                                .then()
+                                .statusCode(201)
+                                .log().all();
+        }
 
+        // other submmit order with auth
+        @Test
+        public void testSubmitOrder2() {
+                String body = """
+                                {
+                                 "bookId": 4,
+                                 "customerName": "Tutoni"
+                                }
+                                    """;
 
+                given().baseUri("https://simple-books-api.click")
+                                .basePath("/orders")
+                                .contentType(ContentType.JSON)
+                                .auth().oauth2(token) // other way to pass the token in the header of authorization with
+                                                      // the format "Bearer " + token
+                                .body(body)
+                                .when()
+                                .post()
+                                .then()
+                                .statusCode(201)
+                                .log().all();
+        }
 
-//otrer submmit order
-@Test
-public void testSubmitOrder2() {
-        String body = """
-                {
-                 "bookId": 4,
-                 "customerName": "Tutoni"
-                }
-                    """;
+        // get an order with the token obtained in the previous test
+        @Test
+        public void testGetOrder() {
+                String orderID = "Gku2S33oaqF2NzUz3aDoo";
+                Response response = given()
+                                .baseUri("https://simple-books-api.click")
+                                .pathParam("OrderId", orderID) // es simplemente un parametro como valor
+                                .basePath("/orders/{OrderId}") // endpoint con el path parameter
+                                .contentType(ContentType.JSON)
+                                .header("Authorization", "Bearer " + token)
+                                .when()
+                                .get()
+                                .then()
+                                .statusCode(200)
+                                .log().all().extract().response();
 
-        given().baseUri("https://simple-books-api.click")
-                .basePath("/orders")
-                .contentType(ContentType.JSON)
-               .auth().oauth2(token) //otra forma de pasar el token, sin necesidad de escribir el header de authorization, el metodo oauth2 se encarga de generar el header con el formato correcto "Bearer " + token
-                .body(body)
-                .when()
-                .post()
-                .then()
-                .statusCode(201)
-                .log().all();
-    }
+        }
 
+        // request with query an pathparameter
+        @Test
+        public void testGetOrdersWithQueryAndPathParam() {
+                String orderID = "Gku2S33oaqF2NzUz3aDoo";
+                Response response = given()
+                                .baseUri("https://simple-books-api.click")
+                                .queryParam("limit", 10) // query parameter para limitar el numero de resultados a 1
+                                .queryParam("type","fiction")                                
+                                .basePath("/books") // endpoint con el path parameter
+                                .contentType(ContentType.JSON)
+                                .header("Authorization", "Bearer " + token)
+                                .when()
+                                .get()
+                                .then()
+                                .statusCode(200)
+                                .log().all().extract().response();
 
-
+        }
 
 
 }
