48f01a75b45352c14d32b88b2ac6b861d1bc463a
totone-uy1990 <ronalggm@outlook.com>
2026-03-31 21:34:01 -0300

    making auth token

--- src/test/java/auth/AuthToken.java
+++ src/test/java/auth/AuthToken.java
@@ -1,5 +1,41 @@
 package auth;
 
-public class AuthToken {
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+
+import org.testng.annotations.Test;
+import io.restassured.response.Response;public class AuthToken {
     
+
+    Response response;
+
+    @Test
+    public Response obtenerToken() {
+
+        String body = """
+                {
+                 "clientName": "tutoni",
+                 "clientEmail": "Tutoni@gmai.com"
+                }
+                    """;
+
+        // obtencion del token
+
+        response = given().baseUri("https://simple-books-api.click")
+                .basePath("/api-clients/")
+                .header("Authorization", "Bearer " + body)
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when()
+                .post()
+                .then()
+                .statusCode(201)
+                .log().all()
+                .extract()
+                .response();
+
+        return response;
+    }
+
 }
